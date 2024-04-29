41ed5c3db9a3310cd5611dc185590f115a8ba33e
taboadaje <taboadaje@gmail.com>
2024-04-28 21:51:50 -0300

    Exception handling - update

--- src/main/java/com/ensaio/course/services/UserService.java
+++ src/main/java/com/ensaio/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.ensaio.course.entities.User;
 import com.ensaio.course.repositories.UserRepository;
 import com.ensaio.course.services.exceptions.DatabaseException;
 import com.ensaio.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -47,9 +48,14 @@ public class UserService {
     }
 
     public User update (Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+
+            throw new ResourceNotFoundException(id);
+        }
         
     }
 
